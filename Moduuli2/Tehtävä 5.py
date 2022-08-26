12796ecf943acf78fcd06c7ae090a191f275ecde
StudentSamir <sami.aboramadan@gmail.com>
2022-08-26 14:11:41 +0300

    Python

--- Moduuli2/Tehtävä 5.py
+++ Moduuli2/Tehtävä 5.py
@@ -1,1 +1,11 @@
-leivisköt=input("Anna leivisköt:")
+leivisköt_str=input("Anna leivisköt:")
+naulat_str=input("Anna naulat:")
+luodit_str=input("Anna luodit:")
+
+leivisköt=float(leivisköt_str)
+naulat=float(naulat_str)
+luodit=float(luodit_str)
+
+leivisköt=20*32
+naulat=32*13.2
+luodit=13.2
