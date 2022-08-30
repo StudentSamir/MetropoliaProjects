2754bc5b04e5daa28efe891dcd84fbfdba718d88
StudentSamir <sami.aboramadan@gmail.com>
2022-08-30 18:55:58 +0300

    Python

--- Moduuli2/Tehtävä 4.py
+++ Moduuli2/Tehtävä 4.py
@@ -1,15 +1,8 @@
-first=5
-second=8
-third=9
-print(first+second+third)
-print((first+second+third)/3)
+firstnumber=float(input("Anna kokonaisluku:"))
+secondnumber=float(input("Anna kokonaisluku:"))
+thirdnumber=float(input("Anna kokonaisluku:"))
 
-a=6
-b=8
-c=9
+print(firstnumber+secondnumber+thirdnumber)
+print(firstnumber*secondnumber*thirdnumber)
+print((firstnumber+secondnumber+thirdnumber)/3)
 
-a=int(a)
-b=int(b)
-c=int(c)
-
-print(a*b*c)
