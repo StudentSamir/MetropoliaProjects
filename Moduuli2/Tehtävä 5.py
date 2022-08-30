cd97e93f9efb86742b84002017adce4648040d83
StudentSamir <sami.aboramadan@gmail.com>
2022-08-30 10:51:47 +0300

    Python

--- Moduuli2/Tehtävä 5.py
+++ Moduuli2/Tehtävä 5.py
@@ -1,3 +1,5 @@
+import math
+
 leivisköt=float(input("Anna leivisköt:"))
 naulat=float(input("Anna naulat:"))
 luodit=float(input("Anna luodit:"))
@@ -6,11 +8,24 @@ leivisköt_sum=(leivisköt*20)
 naulat_sum=(leivisköt_sum*32+naulat*32)
 luodit_sum=(naulat_sum*13.3+luodit*13.3)
 
-
-
-print(leivisköt_sum)
-print(naulat_sum)
 print(luodit_sum)
+
+weight_kilograms=(luodit_sum/1000)
+roundedkilograms = math.floor(weight_kilograms)
+
+roundedgrams=math.floor(luodit_sum)
+grams=float(roundedkilograms+roundedgrams)
+print(grams)
+
+print("Massa nykymittojen mukaan:")
+print(str(roundedkilograms) +" kilogrammaa"+" grammaa")
+
+
+
+
+
+
+
 
 
 
