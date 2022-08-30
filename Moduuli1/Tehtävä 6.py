66aaa6fe3c59bfbe183b0e3e4dc4e2d6013a9873
StudentSamir <sami.aboramadan@gmail.com>
2022-08-30 19:39:13 +0300

    Python

--- Moduuli1/Tehtävä 6.py
+++ Moduuli1/Tehtävä 6.py
@@ -1,4 +1,24 @@
-import(random)
-print(random.randint(1,9))
+import random
 
-threedigitsnumber=
+firstnumbert=(random.randint(0,9))
+secondnumber=(random.randint(0,9))
+thirdnumber=(random.randint(0,9))
+
+print(firstnumbert)
+print(secondnumber)
+print(thirdnumber)
+
+print( )
+
+first=(random.randint(1,6))
+second=(random.randint(1,6))
+third=(random.randint(1,6))
+fourth=(random.randint(1,6))
+
+print(first)
+print(second)
+print(third)
+print(fourth)
+
+
+
