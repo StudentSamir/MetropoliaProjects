e991e06412aaa42873405111c3fedd088802f04c
StudentSamir <sami.aboramadan@gmail.com>
2022-08-31 16:47:27 +0300

    Python

---
+++ Moduuli3/t.1.py
@@ -0,0 +1,4 @@
+kuha=float(input("Anna kuhan pituus:"))
+remaininglength=(37-kuha)
+if kuha<37:
+    print("Laske kuha takaisin järveen."+" Kuha on alimittainen "+str(remaininglength)+" senttimetrillä.")
