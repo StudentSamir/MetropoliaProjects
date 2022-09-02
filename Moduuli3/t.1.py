0a9e5fc49ff9bc3dfc4a9f93d72f3ad37523c388
StudentSamir <sami.aboramadan@gmail.com>
2022-09-02 13:41:39 +0300

    Python

--- Moduuli3/t.1.py
+++ Moduuli3/t.1.py
@@ -1,4 +1,6 @@
-kuha=float(input("Anna kuhan pituus:"))
-remaininglength=(37-kuha)
+kuha=float(input("Anna kuhan pituus senttimetreinä:"))
+
 if kuha<37:
-    print("Laske kuha takaisin järveen."+" Kuha on alimittainen "+str(remaininglength)+" senttimetrillä.")
+    print("Laske kuha takaisin järveen."+" Kuha on alimittainen "+str(37-kuha)+" senttimetrillä.")
+if kuha>37:
+    print("Hieno saalis!")
--- Moduuli4/tehtävä.3.py
+++ Moduuli4/tehtävä.3.py
@@ -1,5 +1,10 @@
 number=str(input("Anna lukuarvo:"))
 while number != "":
+    print("Enter new number")
+    number = str(input("Anna lukuarvo:"))
+    print("pienin ja suurin kluku")
+
+
 
 
 
