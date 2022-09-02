3edf9be71a44a5ae98e91609dd6ff11f078b483c
StudentSamir <sami.aboramadan@gmail.com>
2022-09-02 13:43:54 +0300

    Python

--- Moduuli3/t.1.py
+++ Moduuli3/t.1.py
@@ -2,5 +2,5 @@ kuha=float(input("Anna kuhan pituus senttimetreinä:"))
 
 if kuha<37:
     print("Laske kuha takaisin järveen."+" Kuha on alimittainen "+str(37-kuha)+" senttimetrillä.")
-if kuha>37:
+else:
     print("Hieno saalis!")
