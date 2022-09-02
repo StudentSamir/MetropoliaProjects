8a90e0d268e8552b4943b7ea8ed8a991c26c2489
StudentSamir <sami.aboramadan@gmail.com>
2022-09-02 13:59:56 +0300

    Python

--- Moduuli3/t.2.py
+++ Moduuli3/t.2.py
@@ -1,15 +1,17 @@
 hyttiluokka=input("Anna hyttiluokkasi:")
-hyttiluokka1=str("LUX")
-hyttiluokka2=str("A")
-hyttiluokka3=str("B")
-hyttiluokka4=str("C")
-if hyttiluokka==hyttiluokka1:
-    print(str(hyttiluokka1)+" on parvekkeellinen hytti yläkannella.")
-elif hyttiluokka==hyttiluokka2:
-    print(str(hyttiluokka2)+ " on ikkunallinen hytti autokannen yläpuolella.")
-elif hyttiluokka==hyttiluokka3:
-    print(str(hyttiluokka3)+" on ikkunaton hytti autokannen alapuolella.")
-elif hyttiluokka==hyttiluokka4:
-    print(str(hyttiluokka4)+" on ikkunaton hytti autokannen alapuolella")
+
+#hyttiluokka1=str("LUX")
+#hyttiluokka2=str("A")
+#hyttiluokka3=str("B")
+#hyttiluokka4=str("C")
+
+if hyttiluokka==("LUX"):
+    print(hyttiluokka+" on parvekkeellinen hytti yläkannella.")
+elif hyttiluokka==("A"):
+    print(hyttiluokka+ " on ikkunallinen hytti autokannen yläpuolella.")
+elif hyttiluokka==("B"):
+    print(hyttiluokka+" on ikkunaton hytti autokannen alapuolella.")
+elif hyttiluokka==("C"):
+    print(hyttiluokka+" on ikkunaton hytti autokannen alapuolella")
 else:
     print("Virheellinen hyttiluokka.")
