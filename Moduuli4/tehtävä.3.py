4eca9c7caab3af22cc5f63112f26de6a113b8602
StudentSamir <sami.aboramadan@gmail.com>
2022-09-02 13:45:46 +0300

    Python

--- Moduuli4/tehtävä.3.py
+++ Moduuli4/tehtävä.3.py
@@ -2,4 +2,4 @@ number=str(input("Anna lukuarvo:"))
 while number != "":
     print("Enter new number")
     number = str(input("Anna lukuarvo:"))
-    print("pienin ja suurin luku")
+#   print("pienin ja suurin luku")
