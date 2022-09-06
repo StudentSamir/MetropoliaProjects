2adf9b9e798b7938e5d4d95a33d36dc2dcfaa961
StudentSamir <sami.aboramadan@gmail.com>
2022-09-06 20:22:47 +0300

    Python

--- Moduuli4/tehtävä.5.py
+++ Moduuli4/tehtävä.5.py
@@ -5,12 +5,11 @@ attempts=0
 while   user!=str("python") and password!= str("rules"):
 
     print("Pääsy evätty!")
+    attempts = attempts + 1
     if attempts == 5:
         break
     user = input("Anna käyttäjätunnus:")
     password = input("Anna salasana:")
-
-    attempts = attempts + 1
 
 if user == str("python") and password == str("rules"):
     print("Tervetuloa!")
