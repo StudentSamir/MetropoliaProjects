87dba83b31ec5a6ff4ea3b23a04a7052e8df2e35
StudentSamir <sami.aboramadan@gmail.com>
2022-09-06 22:54:22 +0300

    Python

--- Moduuli4/tehtävä.3.py
+++ Moduuli4/tehtävä.3.py
@@ -1,10 +1,10 @@
 number=str(input("Anna lukuarvo:"))
 
-while number != (""):
+maximum_value = max(number)
+minimum_value = min(number)
 
-    maximum_value = max(number)
-    minimum_value = min(number)
+while number != (""):
 
     number = str(input("Anna lukuarvo:"))
 
-print("Pienin luku on "+str(maximum_value)+" ja"+" suurin luku on "+str(minimum_value)+"!")
+print("Pienin luku on "+str(minimum_value)+" ja"+" suurin luku on "+str(maximum_value)+"!")
