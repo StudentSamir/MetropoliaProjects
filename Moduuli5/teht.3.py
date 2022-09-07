5a45a9388946031bff8629c6d656c124966c1076
StudentSamir <sami.aboramadan@gmail.com>
2022-09-07 20:19:55 +0300

    Python

--- Moduuli5/teht.3.py
+++ Moduuli5/teht.3.py
@@ -2,7 +2,7 @@ number=int(input("Anna kokonaisluku:"))
 
 for n in range(2,number):
     if number % n== 0:
-        print("Luku "+str(number) + " 4ei ole alkuluku!")
+        print("Luku "+str(number) + " ei ole alkuluku!")
         break
 else:
     print("Luku "+str(number) + " on alkuluku!")
