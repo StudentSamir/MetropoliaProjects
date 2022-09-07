0333516ed7868cba9466d4f45f30d7f70483c8af
StudentSamir <sami.aboramadan@gmail.com>
2022-09-07 18:47:32 +0300

    Python

--- Moduuli5/teht.2.py
+++ Moduuli5/teht.2.py
@@ -6,7 +6,6 @@ while number!="":
     numbers_string.append(number)
     number = input("Anna lukuarvo:")
 
-
 numbers_string.sort(reverse=True)
 print("Viisi suurinta lukua ovat:"+str(numbers_string[0:5]))
 
