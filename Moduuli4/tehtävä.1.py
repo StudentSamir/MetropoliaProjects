3f92a042f5c83fabac42820faa80f0efbdd4d4d1
StudentSamir <sami.aboramadan@gmail.com>
2022-09-09 14:09:18 +0300

    Python

--- Moduuli4/tehtävä.1.py
+++ Moduuli4/tehtävä.1.py
@@ -3,3 +3,8 @@ while 1 <= number <=1000:
     number = number + 1
     if number % 3==0:
         print(str(number))
+
+#number=3
+#while 1 <= number <=1000:
+#        print(str(number))
+#        number = number + 3
---
+++ Moduuli6/teht_33.py
@@ -0,0 +1,9 @@
+gallons=float(input("Anna lukuarvo:"))
+def convertor():
+    return(3.79*gallons)
+
+while gallons >= 0:
+    galloona = convertor()
+    print(str(gallons)+ " galloonaa on litroina "+ str(galloona)+ "l")
+    gallons = float(input("Anna lukuarvo:"))
+print("Ohjelma pysäytetty!")
