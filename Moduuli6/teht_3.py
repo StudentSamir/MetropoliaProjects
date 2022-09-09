d66857e1495de6a65070e44d1112ec6fa9d002c7
StudentSamir <sami.aboramadan@gmail.com>
2022-09-09 14:47:16 +0300

    Python

--- Moduuli6/teht_3.py
+++ Moduuli6/teht_3.py
@@ -7,15 +7,3 @@ while gallons >= 0:
     print(str(gallons)+ " galloonaa on litroina "+ str(galloona)+ "l")
     gallons = float(input("Anna lukuarvo:"))
 print("Ohjelma pysäytetty!")
-
-
-
-
-
-
-
-
-
-
-
-
