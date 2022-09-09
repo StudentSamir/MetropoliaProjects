7672e647bf96a0d0113c46d76e9ee34936ec1016
StudentSamir <sami.aboramadan@gmail.com>
2022-09-09 10:26:29 +0300

    Python

---
+++ Moduuli5/teht.4.py
@@ -0,0 +1,11 @@
+kaupungit_list= []
+
+kaupungit= input("Anna viiden kaupungit nimet:")
+
+while kaupungit!=(""):
+    kaupungit_list.append(kaupungit)
+    kaupungit= input("Anna viiden kaupungit nimet:")
+
+for n in kaupungit_list:
+    print (n)
+
