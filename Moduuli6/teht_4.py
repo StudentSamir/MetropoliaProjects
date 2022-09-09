057dca828efdc4d27fc93f4eeb31121ea45e74de
StudentSamir <sami.aboramadan@gmail.com>
2022-09-09 16:26:54 +0300

    Python

--- Moduuli6/teht_4.py
+++ Moduuli6/teht_4.py
@@ -1,5 +1,19 @@
 
+def calculator(numbers_int):
+    sum_x = sum(numbers_int)
+    return sum_x
 
 
+numbers_string = []
+numbers_int = []
 
+number = input("Anna lukuarvo:")
+while number != (""):
+    numbers_string.append(number)
+    number = input("Anna lukuarvo:")
+
+for n in numbers_string:
+    numbers_int.append(int(n))
+
+print("Summa on "+str(calculator(numbers_int)))
 
