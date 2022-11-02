6f10fca1521a3badbebe3280681ab3e08a9a6d7f
StudentSamir <sami.aboramadan@gmail.com>
2022-11-03 00:45:52 +0200

    Python

--- Moduuli10/teht4.py
+++ Moduuli10/teht4.py
@@ -21,52 +21,45 @@ class Auto:
             self.kuljettu_matka += self.nopeus * tunnit
 
 
+
 autot = []
 for k in range (1,10):
     auto = Auto(f"ABC-{k + 1}",randint(100, 200))
     autot.append(auto)
 
 
-
 class Kilpailu:
+
 
     def __init__(self,kilpailu_nimi, pituus_km, autolista):
             self.kilpailu_nimi = kilpailu_nimi
             self.pituus_km = pituus_km
             self.autolista = autolista
-            self.os_auto_lista = []
-            self.tunti = 0
-
-            for n in range(autolista):
-                autolista = Auto((f"ABC-{n + 1}"), randint(100, 200))
-                self.os_auto_lista.append(autolista)
-
-
-    def kilpailu_ohi(self):
-
-        kilpailu2 = True
-
-        while kilpailu2:
-            for auto in autot:
-                if auto.kuljettu_matka > 8000:
-
-                    kilpailu2 = False
-
 
     def tunti_kuluu(self):
 
-        self.tunti += 1
-        for autolista in os_auto_lista:
-            autolista.kiihtyvyys(randint(-10, 15))
-            autolista.kulje(1)
+        for auto in self.autolista:
+            auto.kiihtyvyys(randint(-10, 15))
+            auto.kulje(1)
 
     def tulosta_tilanne(self):
 
         taulukko2 = []
         for auto in autot:
-            taulukko2.append([auto.rekisteritunnus, auto.huippunopeus,auto.nopeus, auto.kuljettu_matka,self.tunti])
+            taulukko2.append([auto.rekisteritunnus, auto.huippunopeus,auto.nopeus, auto.kuljettu_matka])
 
         taulukko2.sort(key=lambda k: k[1], reverse=True)
         print(taulukko2)
 
-kilpailu3= Kilpailu("Suuri romuralli",8000, 10)
+
+kilpailu2 = True
+
+while kilpailu2:
+    for auto in autot:
+        auto.kiihtyvyys(randint(-10, 15))
+        auto.kulje(1)
+        if auto.kuljettu_matka > 8000:
+            kilpailu2 = False
+
+kilpailu22= Kilpailu("Suuri romuralli",8000, 10)
+kilpailu22.tulosta_tilanne()
