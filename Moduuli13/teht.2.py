a5518a905e00c32ddec930ea5837e92920450285
StudentSamir <sami.aboramadan@gmail.com>
2022-12-01 23:19:48 +0200

    Python

--- Moduuli13/teht.2.py
+++ Moduuli13/teht.2.py
@@ -1,26 +1,33 @@
-from flask import Flask, request
-import json
+from flask import Flask
 import mysql.connector
-
-app = Flask(__name__)
-
-yhteys = mysql.connector.connect(
+def database(icao):
+    yhteys = mysql.connector.connect(
     host='127.0.0.1',
     port=3306,
     database='flight_game',
     user='root',
     password='ronaldo',
-    autocommit=True
-)
+    autocommit=True)
+
+    tuple = (icao,)
+    sql = '''SELECT name, municipality FROM airport
+    WHERE ident = %s'''
+    kursori = yhteys.cursor()
+    kursori.execute(sql, tuple)
+    tulos = kursori.fetchone()
+    return tulos
+
+app = Flask(__name__)
 
 @app.route('/kentta/<icao>')
 def kentta(icao):
-    sql = f'select ident, name, municipality from airport where ident = "{icao}"'
-    kursori = yhteys.cursor()
-    kursori.execute(sql)
-    tulos = kursori.fetchone()
+    database(icao)
 
-    return (tulos)
+    answer = {
+        "ICAO": icao, "Name": database(icao)[0], "municipality": database(icao)[1]
+    }
+
+    return answer
 
 if __name__ == '__main__':
     app.run(use_reloader=True, host='127.0.0.1', port=3000)
