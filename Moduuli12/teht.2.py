951e0b702a6c28b5aa00a9cd8f7ca6030b7573cb
StudentSamir <sami.aboramadan@gmail.com>
2023-02-11 13:44:43 +0200

    Python

---
+++ Moduuli12/teht.2.py
@@ -0,0 +1,15 @@
+import requests
+from geopy.geocoders import Nominatim
+
+APIKEY = "YOUR APIKEY"
+user_location = input(" Enter your location: ")
+
+geolocator = Nominatim(user_agent = "api", timeout = 3)
+location = geolocator.geocode(str(user_location))
+
+request = "https://api.openweathermap.org/data/2.5/weather?lat=" + str(location.latitude) + "&lon=" + str(
+    location.longitude) + "&appid=" + APIKEY + "&units=metric"
+
+answer = requests.get(request).json()
+print(answer["weather"][0]["main"])
+print("Temperature: "+str(answer["main"]["temp"]) + "°C")
